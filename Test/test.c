55eadfc9d4ca3436fcbd3d72f50ce26f6d6341ec
Kunal Lanjewar <kunallanjewar@gmail.com>
2015-10-02 09:02:46 -0700

    testing code

--- Test/test.c
+++ Test/test.c
@@ -1,7 +1,2 @@
-#include<stdio.h>
-int main()
-{
- char name[] = "Your Name";
- printf("Hello, %s\n", name);
- printf("Info: %d %c %p\n", name[0], name[2], &name);
-}
+#ifndef __ACCOUNT_H__ #define __ACCOUNT_H__ #define USERNAME_LENGTH 20 #define PIN_LENGTH 8 #define OFFSET 1000000 2.a)typedef struct { char username[USERNAME_LENGTH]; int account; short version; union { int old; char new[PIN_LENGTH]; } pin; } account_t; 2.b)
+int translate_account (int n);2.c)void import_account (int * n); void print_account(account_t * acct); #endif
